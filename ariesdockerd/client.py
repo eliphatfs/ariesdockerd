2f4108e741e9e16bc372c68f2a559f93d58ee5c1
eliphatfs <flandre@scarletx.cn>
2024-02-24 18:09:12 -0800

    Basic client shell.

--- ariesdockerd/client.py
+++ ariesdockerd/client.py
@@ -1,14 +1,16 @@
 import os
 import json
 import asyncio
+import tabulate
 import aioconsole
 import websockets
+from typing import Optional
 from .protocol import client_serial
 
 
 async def first_time_config():
-    addr = input("Input Server Address> ")
-    token = input("Input Token> ")
+    addr = await aioconsole.ainput("Input Server Address> ")
+    token = await aioconsole.ainput("Input Token> ")
     os.makedirs(os.path.expanduser("~/.aries"), exist_ok=True)
     with open(os.path.expanduser("~/.aries/config.json"), "w") as fo:
         json.dump(dict(
@@ -18,6 +20,59 @@ async def first_time_config():
     print("Config Saved to", os.path.expanduser("~/.aries/config.json"))
 
 
+def resp(result):
+    if result['code'] == 0:
+        print("[done]")
+    else:
+        print("[error]", result.get('code', '-2'), result.get('msg', 'unknown'))
+
+
+async def nodes(show_jobs=False):
+    r = await client_serial(ws, 'nodes', dict())
+    if r['code'] == 0:
+        header = ['Node', 'Free GPUs']
+        if show_jobs:
+            header.append('Running')
+        table = []
+        for name, info in r['nodes'].items():
+            row = [name, ','.join(map(str, info['free_gpu_ids']))]
+            if show_jobs:
+                row.append('\n'.join(info['names']))
+            table.append(row)
+        print(tabulate.tabulate(table, headers=header))
+    return r
+
+
+async def ps(filt: Optional[str] = None):
+    r = await client_serial(ws, 'ps', dict(filt=filt))
+    if r['code'] == 0:
+        header = ['ID', 'Name', 'Status', 'User', 'GPUs']
+        table = []
+        for k, v in r['containers']:
+            table.append([k, v['name'], v['status'], v['user'], ','.join('gpu_ids')])
+        print(tabulate.tabulate(table, headers=header))
+    return r
+
+
+async def logs(container: str):
+    r = await client_serial(ws, 'logs', dict(container=container))
+    if r['code'] == 0:
+        print(r['logs'])
+    return r
+
+
+async def stop(container: str):
+    return await client_serial(ws, 'stop', dict(container=container))
+
+
+async def delete(container: str):
+    return await client_serial(ws, 'delete', dict(container=container))
+
+
+async def run(name: str, image: str, cmd: str, n_gpus: int, n_jobs: Optional[int] = None):
+    return await client_serial(ws, 'run', dict(name=name, image=image, cmd=cmd, n_gpus=n_gpus, n_jobs=n_jobs))
+
+
 async def main():
     global ws
     if not os.path.exists(os.path.expanduser("~/.aries/config.json")):
@@ -25,9 +80,23 @@ async def main():
     with open(os.path.expanduser("~/.aries/config.json")) as fi:
         cfg = json.load(fi)
     ws = await websockets.connect(cfg['addr'])
-    await client_serial(ws, 'auth', dict(token=cfg['token']))
-    while True:
-        await eval(await aioconsole.ainput('aries> '))
+    try:
+        auth = await client_serial(ws, 'auth', dict(token=cfg['token']))
+        if auth['code'] != 0:
+            print('[error] login failed:', auth['msg'])
+        print('logged in as', auth['user'])
+        while True:
+            try:
+                cmd = await aioconsole.ainput('aries> ')
+                if cmd in ['nodes', 'ps']:
+                    cmd = cmd + '()'
+                if cmd == 'quit':
+                    break
+                resp(await eval(cmd))
+            except Exception as exc:
+                print("[error]", repr(exc))
+    finally:
+        await ws.close()
 
 
 def sync_main():
